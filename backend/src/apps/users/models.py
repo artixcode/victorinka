e7689fd2637e152ecb18f20e527cda757e6b26c2
Daniil <ctyccy1@gmail.com>
2025-10-12 18:01:49 +0300

    feat(users): add custom User model
    Пользователь, авторизация по email

--- backend/src/apps/users/models.py
+++ backend/src/apps/users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .managers import UserManager
 
-# Create your models here.
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, db_index=True)
+
+    full_name = models.CharField(max_length=150, blank=True)
+    is_email_verified = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
