83933c375eefcc721d7ee957cec8d0cf9715518e
Frant1ccc <ya.mihailovroman201488@gmail.com>
2025-11-25 15:01:27 +0300

    feat(tests)
    added tests for rooms, users, questions

--- backend/src/conftest.py
+++ backend/src/conftest.py
@@ -1,29 +1,35 @@
+# conftest.py
+
 import pytest
-from django.conf import settings
-from rest_framework.test import APIClient
 from model_bakery import baker
+from django.contrib.auth.hashers import make_password
+from rest_framework.test import APIClient
+from django.conf import settings
 
 @pytest.fixture
 def api():
     return APIClient()
 
-@pytest.fixture
-def user(db):
-    return baker.make(settings.AUTH_USER_MODEL, email="u@example.com", nickname="user1", is_active=True, password=None)
 
 @pytest.fixture
-def user_with_password(db, django_user_model):
-    user = django_user_model.objects.create_user(email="p@example.com", password="pass12345", nickname="puser")
-    return user
+@pytest.mark.django_db
+def user():
+    return baker.make(
+        settings.AUTH_USER_MODEL,
+        email="u@example.com",
+        nickname="user1",
+        is_active=True,
+        password=make_password("12345test")
+    )
+
 
 @pytest.fixture
-def auth_client(api, user_with_password):
-    res = api.post("/api/auth/login/", {"email": "p@example.com", "password": "pass12345"}, format="json")
-    assert res.status_code == 200, res.data
-    token = res.data["access"]
-    api.credentials(HTTP_AUTHORIZATION=f"Bearer {token}")
+@pytest.mark.django_db
+def auth_client(api, user):
+    res = api.post("/api/auth/login/", {
+        "email": "u@example.com",
+        "password": "12345test"
+    })
+
+    api.credentials(HTTP_AUTHORIZATION=f"Bearer {res.data['access']}")
     return api
-
-@pytest.fixture
-def author(api, django_user_model):
-    return django_user_model.objects.create_user(email="author@example.com", password="author12345", nickname="author")
